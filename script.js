988e885a84a75b1cb8be87ec1671a352ab64f199
aaaaa18801880-sketch <aaaaa18801880@gmail.com>
2026-08-28 23:15:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-// 상단 메뉴 클릭 시 부드러운 스크롤 이동
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         
-        const targetId = this.getAttribute('href').substring(1);
+        const targetId = this.getAttribute('href').replace('#', '');
         const targetElement = document.getElementById(targetId);
         
         if (targetElement) {
